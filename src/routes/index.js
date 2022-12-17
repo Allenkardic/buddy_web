2947822ed989246763a418972b0d71e083c00bf6
aliakwe <aliakwe@yahoo.com>
2022-12-17 02:28:52 +0100

    added routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,31 +4,60 @@ import {
   Navigate,
   useNavigate,
   useLocation,
+  createRoutesFromElements,
+  createBrowserRouter,
+  BrowserRouter,
 } from "react-router-dom";
 import { Dashboard } from "../pages/dashboard";
 import { Login } from "../pages/login";
 import ErrorPage from "../pages/errorPage";
+import { PrivateRoute } from "./privateRoute";
+import { SideBar } from "../components";
 
-function Route() {
+// function AppRoute() {
+//   return (
+//     <div className='app-container bg-light'>
+//       {/* <Nav /> */}
+//       <div className='container pt-4 pb-4'>
+//         <Routes>
+//           <Route
+//             path='/'
+//             element={
+//               <PrivateRoute>
+//                 <Dashboard />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route path='/login' element={<Login />} />
+//           <Route path='*' element={<Navigate to='/' />} />
+//         </Routes>
+//       </div>
+//     </div>
+//   );
+// }
+
+function AppRoute() {
+  const authUser = true;
   return (
-    <div className='app-container bg-light'>
-      <Nav />
-      <div className='container pt-4 pb-4'>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route path='/login' element={<Login />} />
-          <Route path='*' element={<Navigate to='/' />} />
-        </Routes>
-      </div>
+    <div>
+      {authUser && <SideBar />}
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route
+          path='/dashboard'
+          element={
+            <PrivateRoute>
+              {/* <RoutesContainer /> */}
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path='*' element={<Navigate to='/404' replace />} />
+        <Route path='/404' element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
 
-export default Route;
+export default AppRoute;
